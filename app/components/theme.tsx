7f4c4673601ee887cc7c24d61f24f311b1464036
Micael Bissoni <micael.bissoni@e-thinkers.com.br>
2019-10-13 17:05:58 -0300

    Leiaute index usando Material-UI.

---
+++ app/components/theme.tsx
@@ -0,0 +1,22 @@
+﻿import { createMuiTheme } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/red";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1171a8"
+    },
+    secondary: {
+      main: "#19857b"
+    },
+    error: {
+      main: red.A400
+    },
+    background: {
+      default: "#fff"
+    }
+  }
+});
+
+export default theme;
---
+++ app/pages/_app.tsx
@@ -0,0 +1,44 @@
+﻿import App from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../components/theme";
+
+export default class MyApp extends App {
+  renderHead() {
+    return (
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+      </Head>
+    );
+  }
+  
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode!.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Fragment>
+        <Head>
+          <title>EASY CARROS</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Fragment>
+    );
+  }
+}
